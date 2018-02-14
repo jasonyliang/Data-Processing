8dc8bd2fb6cf3e03860420fe7c05d2ab66c12992
Jason Liang <jjjasonliang@gmail.com>
2018-02-13 17:13:48 -0800

    R simple linear regression final

--- Machine Learning A-Z/Part 2 - Regression/Section 4 - Simple Linear Regression/Simplelinearregression.R
+++ Machine Learning A-Z/Part 2 - Regression/Section 4 - Simple Linear Regression/Simplelinearregression.R
@@ -13,3 +13,36 @@ test_set = subset(dataset, split == FALSE)
 # Feature Scaling
 # training_set = scale(training_set)
 # test_set = scale(test_set)
+
+# Fitting Simple Linear Regression to the Training Set
+regressor = lm(formula = Salary ~ YearsExperience, 
+               data = training_set)
+
+# Predicting the Test set results
+y_pred = predict(regressor, newdata = test_set)
+
+# Visualizing the Training set results
+# install.packages('ggplot2')
+library(ggplot2)
+ggplot() + 
+  geom_point(aes(x = training_set$YearsExperience, y= training_set$Salary),
+             color = 'red') +
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
+            color = 'blue') +
+  ggtitle('Salary vs Experience (Training set)') +
+  xlab('Years of Experience') +
+  ylab('Salary')
+
+# Visualizing the Test set results
+ggplot() + 
+  geom_point(aes(x = test_set$YearsExperience, y= test_set$Salary),
+             color = 'orange') +
+  geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
+            color = 'green') +
+  ggtitle('Salary vs Experience (Test set)') +
+  xlab('Years of Experience') +
+  ylab('Salary')
+
+
+
+
