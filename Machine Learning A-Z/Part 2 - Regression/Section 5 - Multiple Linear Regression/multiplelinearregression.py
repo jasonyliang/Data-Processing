307f6df296911fa7abc8881c6aa26e105a4b9940
Jason Liang <jjjasonliang@gmail.com>
2018-02-17 10:57:45 -0800

    Regressor for multiple linear regression

--- Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiplelinearregression.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiplelinearregression.py
@@ -31,4 +31,10 @@ sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)
+y_train = sc_y.fit_transform(y_train) """
+
+# Fitting Multiple Linear Regression to the Training Set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
