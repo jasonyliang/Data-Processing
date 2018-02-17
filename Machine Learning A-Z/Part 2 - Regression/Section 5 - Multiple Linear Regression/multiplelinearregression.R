32a86b48342bab9e33c510dd69366180dae70cdd
Jason Liang <jjjasonliang@gmail.com>
2018-02-17 12:18:03 -0800

    Automatic implementation of Backward Elimination

--- Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiplelinearregression.R
+++ Machine Learning A-Z/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiplelinearregression.R
@@ -49,3 +49,26 @@ regressor = lm(formula = Profit ~ R.D.Spend,
                data = dataset)
 summary(regressor)
 
+
+
+
+
+# Automatic Implementation
+backwardElimination <- function(x, sl) {
+  numVars = length(x)
+  for (i in c(1:numVars)){
+    regressor = lm(formula = Profit ~ ., data = x)
+    maxVar = max(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"])
+    if (maxVar > sl){
+      j = which(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"] == maxVar)
+      x = x[, -j]
+    }
+    numVars = numVars - 1
+  }
+  return(summary(regressor))
+}
+
+SL = 0.05
+dataset = dataset[, c(1,2,3,4,5)]
+backwardElimination(training_set, SL)
+
