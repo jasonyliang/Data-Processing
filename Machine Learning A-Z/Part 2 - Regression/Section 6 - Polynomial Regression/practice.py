a1a7855feb1ddb2a591c0fd743abf02e0d1ea0a1
Jason Liang <jjjasonliang@gmail.com>
2018-02-19 23:04:17 -0800

    Pre processing

---
+++ Machine Learning A-Z/Part 2 - Regression/Section 6 - Polynomial Regression/practice.py
@@ -0,0 +1,19 @@
+# Polynomial Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+np.set_printoptions(threshold=np.nan)
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, -1].values
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train) """
