cdaa6e1177936d4efeeeb2701572c2486001edfa
Jason Liang <jjjasonliang@gmail.com>
2018-02-20 19:40:28 -0800

    Prediction

--- Machine Learning A-Z/Part 2 - Regression/Section 6 - Polynomial Regression/practice.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 6 - Polynomial Regression/practice.py
@@ -43,9 +43,14 @@ plt.show()
 X_grid = np.arange(min(X), max(X), 0.1)
 X_grid =  X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
-plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
+plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
 plt.title("Truth or Bluff (Polynomia Regression)")
 plt.xlabel("Position Level")
 plt.ylabel("Salary")
 plt.show()
 
+# Predicting a new result with Linear Regression
+lin_reg.predict(6.5)
+
+# Predicting a new result with Polynomial Regression
+lin_reg_2.predict(poly_reg.fit_transform(6.5))
