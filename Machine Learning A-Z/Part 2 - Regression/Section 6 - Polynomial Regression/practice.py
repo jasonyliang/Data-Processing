344571130c9b58214a9311cf50a3b780d9c4f64d
Jason Liang <jjjasonliang@gmail.com>
2018-02-20 19:37:01 -0800

    Smoother curve :)

--- Machine Learning A-Z/Part 2 - Regression/Section 6 - Polynomial Regression/practice.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 6 - Polynomial Regression/practice.py
@@ -25,7 +25,7 @@ lin_reg.fit(X, y)
 
 # Fitting Polynomial Regression to the dataset
 from sklearn.preprocessing import PolynomialFeatures
-poly_reg = PolynomialFeatures(degree = 3)
+poly_reg = PolynomialFeatures(degree = 4)
 X_poly = poly_reg.fit_transform(X)
 lin_reg_2 = LinearRegression()
 lin_reg_2.fit(X_poly, y)
@@ -40,8 +40,10 @@ plt.show()
 
 
 # Visualizing the Polynomial Regression Result
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid =  X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
-plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
+plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
 plt.title("Truth or Bluff (Polynomia Regression)")
 plt.xlabel("Position Level")
 plt.ylabel("Salary")
