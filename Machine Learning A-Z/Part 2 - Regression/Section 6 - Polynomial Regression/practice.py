cefb24b31cb23deaa28023ede4a688c848646040
Jason Liang <jjjasonliang@gmail.com>
2018-02-20 19:32:28 -0800

    Visualize the model

--- Machine Learning A-Z/Part 2 - Regression/Section 6 - Polynomial Regression/practice.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 6 - Polynomial Regression/practice.py
@@ -25,7 +25,25 @@ lin_reg.fit(X, y)
 
 # Fitting Polynomial Regression to the dataset
 from sklearn.preprocessing import PolynomialFeatures
-poly_reg = PolynomialFeatures(degree = 2)
+poly_reg = PolynomialFeatures(degree = 3)
 X_poly = poly_reg.fit_transform(X)
 lin_reg_2 = LinearRegression()
 lin_reg_2.fit(X_poly, y)
+
+# Visualizing the Linear Regression Result
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg.predict(X), color = 'blue')
+plt.title("Truth or Bluff (Linear Regression)")
+plt.xlabel("Position Level")
+plt.ylabel("Salary")
+plt.show()
+
+
+# Visualizing the Polynomial Regression Result
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
+plt.title("Truth or Bluff (Polynomia Regression)")
+plt.xlabel("Position Level")
+plt.ylabel("Salary")
+plt.show()
+
