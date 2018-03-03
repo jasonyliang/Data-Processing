229baf437944fda51028908310d190dde3b63115
Jason Liang <jjjasonliang@gmail.com>
2018-03-03 03:28:04 -0800

    Smooth curve

--- Machine Learning A-Z/Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/practice.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/practice.py
@@ -35,3 +35,14 @@ plt.title('Truth or Bluff (SVR)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
+
+
+# Visualising the SVR results
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (SVR)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
