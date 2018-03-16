5ae56689b6c8f1253d2548c136ec0d6290f967a2
Jason Liang <jjjasonliang@gmail.com>
2018-03-16 10:30:27 -0700

    regressor

--- Machine Learning A-Z/Part 2 - Regression/Section 9 - Random Forest Regression/Practice.R
+++ Machine Learning A-Z/Part 2 - Regression/Section 9 - Random Forest Regression/Practice.R
@@ -1,4 +1,5 @@
 # Random Forest Regression 
+
 # Importing the dataset
 dataset = read.csv('Position_Salaries.csv')
 dataset = dataset[2:3]
@@ -15,25 +16,18 @@ dataset = dataset[2:3]
 # training_set = scale(training_set)
 # test_set = scale(test_set)
 
-# Fitting the Regression Model to the dataset
-# Create your regressor here
+# Fitting the Random Forest Regression to the dataset
+library(randomForest)
+regressor = randomForest(x = dataset[1],#Need a data frame instead of a vector
+                         y = dataset$Salary, #This requires a vector
+                         ntree = 10
+                         )
+
 
 # Predicting a new result
 y_pred = predict(regressor, data.frame(Level = 6.5))
 
-# Visualising the Regression Model results
-# install.packages('ggplot2')
-library(ggplot2)
-ggplot() +
-  geom_point(aes(x = dataset$Level, y = dataset$Salary),
-             colour = 'red') +
-  geom_line(aes(x = dataset$Level, y = predict(regressor, newdata = dataset)),
-            colour = 'blue') +
-  ggtitle('Truth or Bluff (Regression Model)') +
-  xlab('Level') +
-  ylab('Salary')
-
-# Visualising the Regression Model results (for higher resolution and smoother curve)
+# Visualising the Random Forest Regression results (for higher resolution and smoother curve)
 # install.packages('ggplot2')
 library(ggplot2)
 x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
@@ -42,6 +36,6 @@ ggplot() +
              colour = 'red') +
   geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))),
             colour = 'blue') +
-  ggtitle('Truth or Bluff (Regression Model)') +
+  ggtitle('Truth or Bluff (Random Forest Regression)') +
   xlab('Level') +
   ylab('Salary')
