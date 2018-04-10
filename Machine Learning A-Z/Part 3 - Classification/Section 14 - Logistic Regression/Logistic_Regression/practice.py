a224906034d8cf2ed8044b68c91a07794e6f49d3
Jason Liang <jjjasonliang@gmail.com>
2018-04-09 22:52:07 -0700

    Predictions

--- Machine Learning A-Z/Part 3 - Classification/Section 14 - Logistic Regression/Logistic_Regression/practice.py
+++ Machine Learning A-Z/Part 3 - Classification/Section 14 - Logistic Regression/Logistic_Regression/practice.py
@@ -23,4 +23,13 @@ X_test = sc_X.transform(X_test)
 #y_train = sc_y.fit_transform(y_train)
 
 #fitting Logistic Regression to the Training Set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state=0)
+classifier.fit(X_train, y_train)
 
+# Predicting the Test set result
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
