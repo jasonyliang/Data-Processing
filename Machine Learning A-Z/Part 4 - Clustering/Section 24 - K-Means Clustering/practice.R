04f5ee6a42aeb23cbe268d85f2354fa706cd4b72
Jason Liang <jjjasonliang@gmail.com>
2018-04-16 20:18:08 -0700

    visualizing

--- Machine Learning A-Z/Part 4 - Clustering/Section 24 - K-Means Clustering/practice.R
+++ Machine Learning A-Z/Part 4 - Clustering/Section 24 - K-Means Clustering/practice.R
@@ -14,3 +14,17 @@ plot(1:10, wcss, type = 'b', main = paste('Clusters of Clients'),
 # Apply k-means to the mall dataset
 set.seed(29)
 kmeans <-  kmeans(X, 5, iter.max = 300, nstart = 10)
+
+# Visualizing the clusters
+ library(cluster)
+clusplot(X,
+         kmeans$cluster,
+         lines = 0,
+         shade = TRUE,
+         color = TRUE,
+         labels = 2,
+         plotchar = FALSE,
+         span = TRUE,
+         main = paste('Clusters of Clients'),
+         xlab = 'Annual Income',
+         ylab = 'Spending Score')
