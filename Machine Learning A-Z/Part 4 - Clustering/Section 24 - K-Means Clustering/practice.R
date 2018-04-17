693d135ac98d5d07b07d98f5d6783cfb46feddb6
Jason Liang <jjjasonliang@gmail.com>
2018-04-16 19:54:06 -0700

    kmeans

---
+++ Machine Learning A-Z/Part 4 - Clustering/Section 24 - K-Means Clustering/practice.R
@@ -0,0 +1,16 @@
+# K-means Cluster
+
+# importing dataset
+dataset <- read.csv("Mall_Customers.csv")
+X <- dataset[4:5]
+
+# Using the elbow method to find the optimal number of clusters
+set.seed(6)
+wcss <- vector()
+for (i in 1:10) wcss[i] <- sum(kmeans(X, i)$withinss)
+plot(1:10, wcss, type = 'b', main = paste('Clusters of Clients'),
+     xlab = 'Number of Clusters', ylab = 'WCSS')
+
+# Apply k-means to the mall dataset
+set.seed(29)
+kmeans <-  kmeans(X, 5, iter.max = 300, nstart = 10)
