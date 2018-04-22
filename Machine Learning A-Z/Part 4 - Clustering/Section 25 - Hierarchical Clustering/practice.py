b7e62edb93fab0dcb0479aff69b1133130e291d9
Jason Liang <jjjasonliang@gmail.com>
2018-04-21 20:58:01 -0700

    dendrogram

---
+++ Machine Learning A-Z/Part 4 - Clustering/Section 25 - Hierarchical Clustering/practice.py
@@ -0,0 +1,18 @@
+# Hiearchical Clustering
+
+# Import library
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Mall_Customers.csv')
+X = dataset.iloc[:, [3, 4]].values
+
+# Using the dendrogram to find the optimal number of clusters
+import scipy.cluster.hierarchy as sch
+dendrogram = sch.dendrogram(sch.linkage(X,method='ward'))
+plt.title('Dendrogram')
+plt.xlabel('Customers')
+plt.ylabel('Euclidean distances')
+plt.show()
