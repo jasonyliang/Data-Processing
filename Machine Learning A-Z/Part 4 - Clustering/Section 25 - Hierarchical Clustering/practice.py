efa18eb4f31796b7b303bd30b739b8a30d4a7cc0
Jason Liang <jjjasonliang@gmail.com>
2018-04-29 16:23:05 -0500

    Visualizing

--- Machine Learning A-Z/Part 4 - Clustering/Section 25 - Hierarchical Clustering/practice.py
+++ Machine Learning A-Z/Part 4 - Clustering/Section 25 - Hierarchical Clustering/practice.py
@@ -19,6 +19,18 @@ plt.show()
 
 # Fitting Hiearchical Clustering to the mall dataset
 from sklearn.cluster import AgglomerativeClustering
-hc = AgglomerativeClustering(n_cluster= 5, affinity = "euclicean", linkage="ward")
+hc = AgglomerativeClustering(n_clusters = 5, affinity = "euclidean", linkage="ward")
+y_hc = hc.fit_predict(X)
 
+# Visualizing the clusters
+plt.scatter(X[y_hc == 0, 0], X[y_hc == 0, 1], s = 100, c = 'red', label = 'Careful', edgecolor = 'black')
+plt.scatter(X[y_hc == 1, 0], X[y_hc == 1, 1], s = 100, c = 'blue', label = 'Standard', edgecolor = 'black')
+plt.scatter(X[y_hc == 2, 0], X[y_hc == 2, 1], s = 100, c = 'green', label = 'Target', edgecolor = 'black')
+plt.scatter(X[y_hc == 3, 0], X[y_hc == 3, 1], s = 100, c = 'cyan', label = 'Careless', edgecolor = 'black')
+plt.scatter(X[y_hc == 4, 0], X[y_hc == 4, 1], s = 100, c = 'magenta', label = 'Sensible', edgecolor = 'black')
 
+plt.title('Clusters of Clients')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend()
+plt.show()
