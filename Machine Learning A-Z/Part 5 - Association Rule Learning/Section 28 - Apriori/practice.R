32f550052ba2ee0b7cfcf7a2cdf086b7c82fd246
Jason Liang <jjjasonliang@gmail.com>
2018-04-30 00:32:29 -0500

    sparse matrix

---
+++ Machine Learning A-Z/Part 5 - Association Rule Learning/Section 28 - Apriori/practice.R
@@ -0,0 +1,11 @@
+# Apriori
+
+# Data Processing 
+library(arules)
+dataset = read.csv('Market_Basket_Optimisation.csv', header=FALSE)
+dataset = read.transactions('Market_Basket_Optimisation.csv', 
+                            sep = ',',
+                            rm.duplicates = TRUE
+                            )
+summary(dataset)
+itemFrequencyPlot(dataset, topN = 100)
