a22db45c4b5ac0da5a6f9fa7a1f902bf418aaa2d
Jason Liang <jjjasonliang@gmail.com>
2018-05-04 13:55:31 -0500

    take out stopwords using nltk library

--- Machine Learning A-Z/Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/practice.py
+++ Machine Learning A-Z/Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/practice.py
@@ -10,5 +10,10 @@ dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
 
 # Cleaning the texts
 import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
 review = re.sub('[^a-zA-Z]',' ', dataset['Review'][0])
 review = review.lower()
+review = review.split()
+review = [word for word in review if not word in set(stopwords.words('english'))] #set makes it faster
