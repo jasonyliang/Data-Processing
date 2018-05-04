56b17dcf18305b7c004eb7ca1001e2aa68c30d0b
Jason Liang <jjjasonliang@gmail.com>
2018-05-04 13:45:00 -0500

    sample text cleaning

--- Machine Learning A-Z/Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/practice.py
+++ Machine Learning A-Z/Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/practice.py
@@ -6,5 +6,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '/t', quoting = 3)
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
 
+# Cleaning the texts
+import re
+review = re.sub('[^a-zA-Z]',' ', dataset['Review'][0])
