f3d0a8d06481ada6742893a16a99db9e2b9777a2
Jason Liang <jjjasonliang@gmail.com>
2018-05-03 22:30:58 -0500

    update

--- Machine Learning A-Z/Part 6 - Reinforcement Learning/Section 33 - Thompson Sampling/practice.py
+++ Machine Learning A-Z/Part 6 - Reinforcement Learning/Section 33 - Thompson Sampling/practice.py
@@ -9,7 +9,7 @@ import pandas as pd
 dataset = pd.read_csv('ADs_CTR_Optimisation.csv')
 
 # Implement Thompson Sampling
-import math
+import random
 N = 10000 # number of rounds
 d = 10 # number of models
 ads_selected = []
@@ -20,18 +20,16 @@ for n in range(0, N):
     ad = 0
     max_random = 0
     for i in range(0, d):
-        if numbers_of_selection[i] > 0:
-            average_reward = sums_of_rewards[i] / numbers_of_selection[i]
-            delta_i = math.sqrt(3/2 * math.log(n + 1) / numbers_of_selection[i])
-            upper_bound = average_reward + delta_i
-        else:
-            upper_bound = 1e400
-        if upper_bound > max_upper_bound:
-            max_upper_bound = upper_bound
+        random_beta = random.betavariate(numbers_of_rewards_1[i] + 1, numbers_of_rewards_0[i] + 1)
+        if random_beta > max_random:
+            max_random = random_beta
             ad = i
     ads_selected.append(ad)
-    numbers_of_selection[ad] += 1
-    sums_of_rewards[ad] += dataset.values[n, ad]
+    reward = dataset.values[n, ad]
+    if reward == 1:
+        numbers_of_rewards_1[ad] += 1
+    else:
+        numbers_of_rewards_0[ad] += 1
     total_reward += dataset.values[n, ad]
 
 # Visualizing the result
