4e7804e011a20801b00d363437957ba063d555bc
Jason Liang <jjjasonliang@gmail.com>
2018-05-04 13:46:15 -0500

    clean part II

--- Machine Learning A-Z/Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/practice.py
+++ Machine Learning A-Z/Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/practice.py
@@ -11,3 +11,4 @@ dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
 # Cleaning the texts
 import re
 review = re.sub('[^a-zA-Z]',' ', dataset['Review'][0])
+review = review.lower()
