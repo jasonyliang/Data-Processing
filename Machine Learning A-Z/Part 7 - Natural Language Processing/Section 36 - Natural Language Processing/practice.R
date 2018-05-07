f406c5e59ec8bb9eed90ade4f5e610330aeaa1e6
Jason Liang <jjjasonliang@gmail.com>
2018-05-06 21:47:17 -0700

    cleaning

--- Machine Learning A-Z/Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/practice.R
+++ Machine Learning A-Z/Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/practice.R
@@ -4,5 +4,11 @@
 dataset = read.delim('Restaurant_Reviews.tsv', quote = '', stringsAsFactors = FALSE)
 
 # Cleaning the text
+#install.packages('SnowballC')
 library(tm)
+library(SnowballC)
 corpus = VCorpus(VectorSource(dataset$Review))
+corpus = tm_map(corpus, content_transformer(tolower))
+corpus = tm_map(corpus, removeNumbers)
+corpus = tm_map(corpus, removePunctuation)
+corpus = tm_map(corpus, removeWords, stopwords())
