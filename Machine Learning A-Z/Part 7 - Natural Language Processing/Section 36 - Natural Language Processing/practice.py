3f4c450b7062d5084561b6d30efa26e45966e286
Jason Liang <jjjasonliang@gmail.com>
2018-05-06 18:10:27 -0700

    Classification

--- Machine Learning A-Z/Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/practice.py
+++ Machine Learning A-Z/Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/practice.py
@@ -29,3 +29,26 @@ from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer(max_features = 1500)
 X = cv.fit_transform(corpus).toarray()
 y = dataset.iloc[:, 1].values
+
+# Classification
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Fitting Naive Bayes to the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
