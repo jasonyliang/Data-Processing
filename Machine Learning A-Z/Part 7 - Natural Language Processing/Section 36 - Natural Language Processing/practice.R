90afe8734843426072fa2ec452d800cc2ca410ab
Jason Liang <jjjasonliang@gmail.com>
2018-05-07 12:13:14 -0700

    Bag of words model + Random Forest

--- Machine Learning A-Z/Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/practice.R
+++ Machine Learning A-Z/Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/practice.R
@@ -1,7 +1,7 @@
 # Natural Language Processing
 
 # Importing dataset
-dataset = read.delim('Restaurant_Reviews.tsv', quote = '', stringsAsFactors = FALSE)
+dataset_original = read.delim('Restaurant_Reviews.tsv', quote = '', stringsAsFactors = FALSE)
 
 # Cleaning the text
 #install.packages('SnowballC')
@@ -18,3 +18,31 @@ corpus = tm_map(corpus, stripWhitespace)
 # Creating the Bag of Words model
 dtm = DocumentTermMatrix(corpus)
 dtm = removeSparseTerms(dtm, 0.999)
+dataset = as.data.frame(as.matrix(dtm))
+dataset$Liked = dataset_original$Liked
+
+# Classification
+# Encoding the target feature as factor
+dataset$Liked = factor(dataset$Liked, levels = c(0, 1))
+
+# Splitting the dataset into the Training set and Test set
+#install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Liked, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Fitting Random Forest Classification to the Training set
+#install.packages('randomForest')
+library(randomForest)
+set.seed(123)
+classifier = randomForest(x = training_set[-692],
+                          y = training_set$Liked,
+                          ntree = 50)
+
+# Predicting the Test set results
+y_pred = predict(classifier, newdata = test_set[-692])
+
+# Making the Confusion Matrix
+cm = table(test_set[, 692], y_pred)
