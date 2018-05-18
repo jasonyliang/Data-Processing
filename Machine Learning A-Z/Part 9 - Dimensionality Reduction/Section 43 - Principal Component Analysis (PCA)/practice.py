d8b3ebe59695e0453ecc5953095ea2447aa4d31b
Jason Liang <jjjasonliang@gmail.com>
2018-05-18 17:29:30 +0800

    Visualize

--- Machine Learning A-Z/Part 9 - Dimensionality Reduction/Section 43 - Principal Component Analysis (PCA)/practice.py
+++ Machine Learning A-Z/Part 9 - Dimensionality Reduction/Section 43 - Principal Component Analysis (PCA)/practice.py
@@ -27,6 +27,7 @@ X_test = sc_X.transform(X_test)
 from sklearn.decomposition import PCA
 pca = PCA(n_components = 2) #start with None to see everything, make sure to restart the kernal
 X_train = pca.fit_transform(X_train)
+X_test = pca.fit_transform(X_test)
 explained_variance = pca.explained_variance_ratio_
 
 #fitting Logistic Regression to the Training Set
@@ -47,15 +48,15 @@ X_set, y_set = X_train, y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min()-1, stop = X_set[:,0].max()+1, step = 0.01),
                     np.arange(start = X_set[:,1].min()-1, stop=X_set[:,1].max()+1,step=0.01))
 plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-                                                 alpha=0.75, cmap=ListedColormap(('red', 'green')))
+                                                 alpha=0.75, cmap=ListedColormap(('red', 'green', 'blue')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j, edgecolor='black')
+                c = ListedColormap(('red', 'green', 'blue'))(i), label = j, edgecolor='black')
 plt.title('Logistic Regression (Training Set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
 plt.legend()
 plt.show()
 
@@ -67,14 +68,14 @@ X_set, y_set = X_test, y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min()-1, stop = X_set[:,0].max()+1, step = 0.01),
                     np.arange(start = X_set[:,1].min()-1, stop=X_set[:,1].max()+1,step=0.01))
 plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-                                                 alpha=0.75, cmap=ListedColormap(('red', 'green')))
+                                                 alpha=0.75, cmap=ListedColormap(('red', 'green', 'blue')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j, edgecolor='black')
+                c = ListedColormap(('red', 'green', 'blue'))(i), label = j, edgecolor='black')
 plt.title('Logistic Regression (Testing Set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
+plt.xlabel('PC1')
+plt.ylabel('PC2')
 plt.legend()
 plt.show()
