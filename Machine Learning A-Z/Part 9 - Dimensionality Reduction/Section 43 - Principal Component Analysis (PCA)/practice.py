c35432a93ea167b1e1c45db1f4fffed0849373e1
Jason Liang <jjjasonliang@gmail.com>
2018-05-18 10:42:13 +0800

    apply pca

--- Machine Learning A-Z/Part 9 - Dimensionality Reduction/Section 43 - Principal Component Analysis (PCA)/practice.py
+++ Machine Learning A-Z/Part 9 - Dimensionality Reduction/Section 43 - Principal Component Analysis (PCA)/practice.py
@@ -8,12 +8,12 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Wine.csv')
-X = dataset.iloc[:, [2,3]].values
-y = dataset.iloc[:, 4].values
+X = dataset.iloc[:, 0:13].values
+y = dataset.iloc[:, 13].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
@@ -22,6 +22,12 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 #sc_y = StandardScaler()
 #y_train = sc_y.fit_transform(y_train)
+
+# Applying PCA
+from sklearn.decomposition import PCA
+pca = PCA(n_components = 2) #start with None to see everything, make sure to restart the kernal
+X_train = pca.fit_transform(X_train)
+explained_variance = pca.explained_variance_ratio_
 
 #fitting Logistic Regression to the Training Set
 from sklearn.linear_model import LogisticRegression
