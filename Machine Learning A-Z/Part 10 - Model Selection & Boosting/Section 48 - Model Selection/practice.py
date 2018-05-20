1aa4f89a7ef2f2b436b1ba74615fc2812190ea2b
Jason Liang <jjjasonliang@gmail.com>
2018-05-20 21:44:39 +0800

    model selection

--- Machine Learning A-Z/Part 10 - Model Selection & Boosting/Section 48 - Model Selection/practice.py
+++ Machine Learning A-Z/Part 10 - Model Selection & Boosting/Section 48 - Model Selection/practice.py
@@ -1,4 +1,4 @@
-# Kernel SVM
+# K-Fold Cross Validation
 
 # Importing the libraries
 import numpy as np
@@ -11,7 +11,7 @@ X = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, 4].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
+from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 # Feature Scaling
@@ -31,13 +31,6 @@ y_pred = classifier.predict(X_test)
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
-
-# Applying K-Fold Cross Validation
-from sklearn.model_selection import cross_val_score
-accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10) 
-    # n_jobs = -1 to make it faster
-accuracies.mean()
-accuracies.std()
 
 # Visualising the Training set results
 from matplotlib.colors import ListedColormap
