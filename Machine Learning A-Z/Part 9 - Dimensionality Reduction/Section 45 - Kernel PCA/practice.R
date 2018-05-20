6a885f7ceb1bf87be33e27f52ae06f0c98c9bbff
Jason Liang <jjjasonliang@gmail.com>
2018-05-20 15:58:28 +0800

    finalize

--- Machine Learning A-Z/Part 9 - Dimensionality Reduction/Section 45 - Kernel PCA/practice.R
+++ Machine Learning A-Z/Part 9 - Dimensionality Reduction/Section 45 - Kernel PCA/practice.R
@@ -26,31 +26,34 @@ kpca = kpca(~.,
             data = training_set[-3],
             kernel = 'rbfdot',
             features = 2)
-
+training_set_pca = as.data.frame(predict(kpca, training_set))
+training_set_pca$Purchased = training_set$Purchased
+test_set_pca = as.data.frame(predict(kpca, test_set))
+test_set_pca$Purchased = test_set$Purchased
 # Fitting Logistic Regression to the Training set
 classifier = glm(formula = Purchased ~ .,
                  family = binomial,
-                 data = training_set)
+                 data = training_set_pca)
 
 # Predicting the Test set results
-prob_pred = predict(classifier, type = 'response', newdata = test_set[-3])
+prob_pred = predict(classifier, type = 'response', newdata = test_set_pca[-3])
 y_pred = ifelse(prob_pred > 0.5, 1, 0)
 
 # Making the Confusion Matrix
-cm = table(test_set[, 3], y_pred > 0.5)
+cm = table(test_set_pca[, 3], y_pred > 0.5)
 
 # Visualising the Training set results
 library(ElemStatLearn)
-set = training_set
+set = training_set_pca
 X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
-colnames(grid_set) = c('Age', 'EstimatedSalary')
+colnames(grid_set) = c('V1', 'V2')
 prob_set = predict(classifier, type = 'response', newdata = grid_set)
 y_grid = ifelse(prob_set > 0.5, 1, 0)
 plot(set[, -3],
      main = 'Logistic Regression (Training set)',
-     xlab = 'Age', ylab = 'Estimated Salary',
+     xlab = 'PC1', ylab = 'PC2',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
 points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
@@ -58,16 +61,16 @@ points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
 
 # Visualising the Test set results
 library(ElemStatLearn)
-set = test_set
+set = test_set_pca
 X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
-colnames(grid_set) = c('Age', 'EstimatedSalary')
+colnames(grid_set) = c('V1', 'V2')
 prob_set = predict(classifier, type = 'response', newdata = grid_set)
 y_grid = ifelse(prob_set > 0.5, 1, 0)
 plot(set[, -3],
      main = 'Logistic Regression (Test set)',
-     xlab = 'Age', ylab = 'Estimated Salary',
+     xlab = 'PC1', ylab = 'PC2',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
 points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
