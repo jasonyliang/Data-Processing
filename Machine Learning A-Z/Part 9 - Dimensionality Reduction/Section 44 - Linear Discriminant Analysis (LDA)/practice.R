43344125a6b855d5a04e9387907c8e723f29b3b4
Jason Liang <jjjasonliang@gmail.com>
2018-05-20 11:53:38 +0800

    LDA

--- Machine Learning A-Z/Part 9 - Dimensionality Reduction/Section 44 - Linear Discriminant Analysis (LDA)/practice.R
+++ Machine Learning A-Z/Part 9 - Dimensionality Reduction/Section 44 - Linear Discriminant Analysis (LDA)/practice.R
@@ -16,12 +16,20 @@ training_set[-14] = scale(training_set[-14])
 test_set[-14] = scale(test_set[-14])
 
 # Applying PCA
+library(MASS)
+lda = lda(formula = Customer_Segment ~ .,
+          data = training_set) # Find two features by default
+# need a dataframe 
+training_set = as.data.frame(predict(object = lda, training_set))
+training_set = training_set[c(5, 6, 1)]
+test_set = as.data.frame(predict(lda, test_set))
+test_set = test_set[c(5, 6, 1)]
 
 
 # Fitting SVM to the Training set
 # install.packages('e1071')
 library(e1071)
-classifier = svm(formula = Customer_Segment ~ .,
+classifier = svm(formula = class ~ .,
                  data = training_set,
                  type = 'C-classification',
                  kernel = 'linear')
@@ -38,11 +46,11 @@ set = training_set
 X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
-colnames(grid_set) = c('PC1', 'PC2')
+colnames(grid_set) = c('x.LD1', 'x.LD2')
 y_grid = predict(classifier, newdata = grid_set)
 plot(set[, -3],
      main = 'SVM (Training set)',
-     xlab = 'PC1', ylab = 'PC2',
+     xlab = 'LD1', ylab = 'LD2',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
 points(grid_set, pch = '.', col = ifelse(y_grid == 2, 'deepskyblue', ifelse(y_grid == 1, 'springgreen3', 'tomato')))
@@ -54,10 +62,10 @@ set = test_set
 X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
-colnames(grid_set) = c('PC1', 'PC2')
+colnames(grid_set) = c('x.LD1', 'x.LD2')
 y_grid = predict(classifier, newdata = grid_set)
 plot(set[, -3], main = 'SVM (Test set)',
-     xlab = 'PC1', ylab = 'PC2',
+     xlab = 'LD1', ylab = 'LD2',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
 points(grid_set, pch = '.', col = ifelse(y_grid == 2, 'deepskyblue', ifelse(y_grid == 1, 'springgreen3', 'tomato')))
