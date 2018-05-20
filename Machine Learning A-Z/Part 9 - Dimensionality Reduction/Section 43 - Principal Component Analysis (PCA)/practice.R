ede3ec31be2d8108d2a671838a64bd2c80a1adc7
Jason Liang <jjjasonliang@gmail.com>
2018-05-20 08:11:49 +0800

    pca with SVM

--- Machine Learning A-Z/Part 9 - Dimensionality Reduction/Section 43 - Principal Component Analysis (PCA)/practice.R
+++ Machine Learning A-Z/Part 9 - Dimensionality Reduction/Section 43 - Principal Component Analysis (PCA)/practice.R
@@ -15,37 +15,47 @@ test_set = subset(dataset, split == FALSE)
 training_set[-14] = scale(training_set[-14])
 test_set[-14] = scale(test_set[-14])
 
-#
+# Apply PCA
+#install.packages('caret')
+library(caret)
+#install.packages('e1071')
+library(e1071)
+pca = preProcess(x = training_set[-14], method = 'pca', pcaComp = 2)
+training_set = predict(pca, training_set)
+training_set = training_set[c(2, 3, 1)] #switch the order of the columns
+test_set = predict(pca, test_set)
+test_set = test_set[c(2, 3, 1)]
 
-# Fitting the Logistic Regression to Training Set
-classifier = glm(formula=Purchased~.,
-                 family = binomial,
-                 data = training_set)
 
-# Predicting the Test Set Result
-prob_pred = predict(classifier, type = "response", newdata = test_set[-3])
-y_pred = ifelse(prob_pred > 0.5, 1, 0)
+# Fitting SVM to the Training set
+# install.packages('e1071')
+library(e1071)
+classifier = svm(formula = Customer_Segment ~ .,
+                 data = training_set,
+                 type = 'C-classification',
+                 kernel = 'linear')
 
+# Predicting the Test set results
+y_pred = predict(classifier, newdata = test_set[-3])
 
 # Making the Confusion Matrix
 cm = table(test_set[, 3], y_pred)
 
-# Visualizing the Training Set Results
+# Visualising the Training set results
 library(ElemStatLearn)
 set = training_set
 X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
-colnames(grid_set) = c('Age', 'EstimatedSalary')
-prob_set = predict(classifier, type = 'response', newdata = grid_set)
-y_grid = ifelse(prob_set > 0.5, 1, 0)
+colnames(grid_set) = c('PC1', 'PC2')
+y_grid = predict(classifier, newdata = grid_set)
 plot(set[, -3],
-     main = 'Logistic Regression (Training set)',
-     xlab = 'Age', ylab = 'Estimated Salary',
+     main = 'SVM (Training set)',
+     xlab = 'PC1', ylab = 'PC2',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
-points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
-points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', ifelse(y_grid == 2, 'tomato', 'deepskyblue')))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', ifelse(set[, 3] == 2, 'red2', 'blue3')))
 
 # Visualising the Test set results
 library(ElemStatLearn)
@@ -53,13 +63,11 @@ set = test_set
 X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
-colnames(grid_set) = c('Age', 'EstimatedSalary')
-prob_set = predict(classifier, type = 'response', newdata = grid_set)
-y_grid = ifelse(prob_set > 0.5, 1, 0)
-plot(set[, -3],
-     main = 'Logistic Regression (Test set)',
-     xlab = 'Age', ylab = 'Estimated Salary',
+colnames(grid_set) = c('PC1', 'PC2')
+y_grid = predict(classifier, newdata = grid_set)
+plot(set[, -3], main = 'SVM (Test set)',
+     xlab = 'PC1', ylab = 'PC2',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
-points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
-points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', ifelse(y_grid == 2, 'tomato', 'deepskyblue')))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', ifelse(set[, 3] == 2, 'red2', 'blue3')))
