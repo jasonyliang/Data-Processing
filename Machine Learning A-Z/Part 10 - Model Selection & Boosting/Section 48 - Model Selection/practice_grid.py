1544e6b0df99793e9fdc3841ad03a7e611962b61
Jason Liang <jjjasonliang@gmail.com>
2018-05-21 09:14:28 +0800

    grid search

--- Machine Learning A-Z/Part 10 - Model Selection & Boosting/Section 48 - Model Selection/practice_grid.py
+++ Machine Learning A-Z/Part 10 - Model Selection & Boosting/Section 48 - Model Selection/practice_grid.py
@@ -1,4 +1,4 @@
-# K-Fold Cross Validation
+# Grid Search
 
 # Importing the libraries
 import numpy as np
@@ -11,7 +11,7 @@ X = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, 4].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 # Feature Scaling
@@ -32,12 +32,26 @@ y_pred = classifier.predict(X_test)
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
-# Applying k-Fold Cross Validation
+# Applying K-Fold Cross Validation
 from sklearn.model_selection import cross_val_score
 accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10) 
     # n_jobs = -1 to make it faster
 accuracies.mean()
 accuracies.std()
+
+# Applying Grid Search to find the best model and the best parameters
+from sklearn.model_selection import GridSearchCV
+parameters = [{'C': [1, 10, 100, 1000], 'kernel' : ['linear']},
+              {'C': [1, 10, 100, 1000], 'kernel' : ['rbf'], 'gamma':[0.5, 0.1, 0.01, 0.001, 0.0001]}
+              ]
+grid_search = GridSearchCV(estimator = classifier,
+                           param_grid = parameters,
+                           scoring = 'accuracy',
+                           cv = 10
+                           ) # could use n_jobs = -1 to make it faster
+grid_search = grid_search.fit(X_train, y_train)
+best_accuracy = grid_search.best_score_
+best_parameters = grid_search.best_params_
 
 
 # Visualising the Training set results
